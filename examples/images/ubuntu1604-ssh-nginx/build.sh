f9b453db2131ff633dcd61468b79ca6a7fd76223
PK <pk@gae123.com>
2017-05-30 23:03:52 -0700

    We need rsync in the image

--- examples/images/ubuntu1604-ssh-nginx/build.sh
+++ examples/images/ubuntu1604-ssh-nginx/build.sh
@@ -2,8 +2,10 @@
 
 set -e   # exit on first error
 
+VERSION=1.1
+
 readonly SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo docker login
-sudo docker build -t pako/ubuntu1604-ssh-nginx:1.0 $SCRIPTPATH
-sudo docker push pako/ubuntu1604-ssh-nginx:1.0
+#sudo docker login
+sudo docker build -t pako/ubuntu1604-ssh-nginx:$VERSION $SCRIPTPATH
+sudo docker push pako/ubuntu1604-ssh-nginx:$VERSION
