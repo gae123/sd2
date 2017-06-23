bb43a30f643ed83af0ac0964d1cb7017e682fa6b
PK <pk@gae123.com>
2017-06-23 12:41:21 -0700

    Dealing better with versioning

--- src/lib/sd2/__init__.py
+++ src/lib/sd2/__init__.py
@@ -5,7 +5,7 @@
 
 from .config import read_config, has_timestamp_changed, sd2_config_schema
 from .myhosts import get_hosts, get_hosts_dict
-from . import gen_all, sd2cont
+from . import gen_all, sd2cont, version
 
 
-__version__='0.9.43'
+__version__=version.version
