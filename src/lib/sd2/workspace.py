1a06549430eb20e2d41db7debc4e32f2b8da92a9
PK <pk@gae123.com>
2017-10-01 01:18:49 -0700

    enabled/disabled at the workspace target level

--- src/lib/sd2/workspace.py
+++ src/lib/sd2/workspace.py
@@ -11,8 +11,19 @@ class Workspace(object):
 
     def get_targets(self):
         hosts = self._node.get('targets', [])
-        rr = [x for x in hosts if myhosts.is_enabled(x['name'])]
-        #rr.extend([x for x in hosts if myhosts.exists_container(x['name']) ])
+
         for host in hosts:
             assert 'name' in host
+
+        rr = [x for x in hosts if myhosts.is_enabled(x['name'])]
+        rr = [x for x in rr if x.get('enabled', True) and not x.get('disabled', False)]
+        #rr.extend([x for x in hosts if myhosts.exists_container(x['name']) ])
         return rr
+
+    def is_enabled(self):
+        if not self._node.get('enabled', True) or self._node.get('disabled', False):
+            return False
+        if not self.get_targets():
+            return False     
+        return True
+
